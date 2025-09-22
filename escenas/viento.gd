ab3d87da6d0c26c0845e849490425ee84dd5e856
Cecilia Fernandez <ceciliafernandez@Cecilias-MacBook-Pro.local>
2025-09-22 13:09:02 -0300

    Agregue voz del peronaje y musica

--- escenas/viento.gd
+++ escenas/viento.gd
@@ -3,5 +3,5 @@ extends Node2D
 var JUEGO = preload("res://escenas/juego.tscn")
 
 func _ready() -> void:
-	await get_tree().create_timer(5).timeout
+	await get_tree().create_timer(6).timeout
 	get_tree().change_scene_to_packed(JUEGO)
